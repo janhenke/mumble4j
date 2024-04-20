e137721485aeb636be23139e24b32ef60b3bcb19
Jan Henke <Jan.Henke@taujhe.de>
2024-04-20 19:04:50 +0200

    Generate Java classes from protobuf

--- core/src/main/protobuf/process_protobuf.sh
+++ core/src/main/protobuf/process_protobuf.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker run --rm -u "$(id -u)" -v"${PWD}":"${PWD}" -w"${PWD}" jaegertracing/protobuf:latest --proto_path="${PWD}" --java_out="${PWD}/../java" -I/usr/include/github.com/gogo/protobuf "${PWD}/Mumble.proto"
-docker run --rm -u "$(id -u)" -v"${PWD}":"${PWD}" -w"${PWD}" jaegertracing/protobuf:latest --proto_path="${PWD}" --java_out="${PWD}/../java" -I/usr/include/github.com/gogo/protobuf "${PWD}/MumbleUDP.proto"
+docker run --rm -u "$(id -u)" -v"${PWD}":"${PWD}" -w"${PWD}" jaegertracing/protobuf:latest --proto_path="${PWD}" --java_out="${PWD}" -I/usr/include/github.com/gogo/protobuf "${PWD}/Mumble.proto"
+mv MumbleProto ../java/
+
+docker run --rm -u "$(id -u)" -v"${PWD}":"${PWD}" -w"${PWD}" jaegertracing/protobuf:latest --proto_path="${PWD}" --java_out="${PWD}" -I/usr/include/github.com/gogo/protobuf "${PWD}/MumbleUDP.proto"
+mv MumbleUDP ../java/
+
