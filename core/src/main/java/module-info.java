3d34e5e05c6e7520839f84280e19019cac489acb
Jan Henke <Jan.Henke@taujhe.de>
2024-05-05 18:38:52 +0200

    Replace explicit module-info.java with automatic module name
    One of our dependencies does not declare a module name yet, so we should not define a module-info.java until that is fixed.

--- core/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Jan Henke (Jan.Henke@taujhe.de)
- */
-module de.taujhe.mumble4j.core {
-	requires org.jetbrains.annotations;
-	requires com.google.protobuf;
-
-	exports de.taujhe.mumble4j.core;
-}
